54812c768b3487a2efef3e615805b026e9b5701a
Chris Antaki <ChrisAntaki@gmail.com>
2014-07-07 01:53:58 -0700

    Added generator

---
+++ primetime.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	int nth = 1000;
+
+	if (string(argv[1]) == "-nth") {
+		nth = atoi(argv[2]);
+	}
+
+	vector<int> primes;
+	primes.push_back(2);
+
+	int i;
+	for (i = 3; primes.size() < nth; i++) {
+		bool prime = true;
+
+		for (int j = 0; j < primes.size() && primes[j]*primes[j] <= i; j++) {
+			if (i % primes[j] == 0) {
+				prime = false;
+				break;
+			}
+		}
+
+		if (prime) {
+			primes.push_back(i);
+		}
+	}
+
+	int final = primes.back();
+	cout << final << "...";
+}
